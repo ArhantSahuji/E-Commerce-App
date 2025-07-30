7624490c9292305f14810634dd42c616b6301e41
Arhant Sahuji <arhantsahuji134@gmail.com>
2025-07-30 14:56:17 +0530

    latest collection section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-        <img src={assets.logo2} className='' alt=''></img>
+        <Link to="/"><img src={assets.logo2} className='' alt=''></img></Link>
 
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
             <NavLink to="/" className={({isActive}) =>`flex flex-col items-center gap-1
@@ -79,7 +79,7 @@ function Navbar() {
                 <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to="/">HOME</NavLink>
                 <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to="/collection">COLLECTION</NavLink>
                 <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to="/about">ABOUT</NavLink>
-                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' ato="/contact">CONTACT</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to="/contact">CONTACT</NavLink>
             </div>
         </div>
 
