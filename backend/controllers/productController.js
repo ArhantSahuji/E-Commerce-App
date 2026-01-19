5865f44ac818b6f6958653752a0dc43b816904fc
Arhant Sahuji <arhantsahuji134@gmail.com>
2026-01-19 20:16:55 +0530

    admin list product functionality and admin pannel add product functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,7 +8,7 @@ const addProduct= async(req,res)=>{
 
     try {
 
-        const {name , description , price , category , subCategory , sizes , bestSeller} = req.body;
+        const {name , description , price , category , subCategory , sizes , bestseller} = req.body;
 
         const image1=req.files.image1 && req.files.image1[0];
         const image2=req.files.image2 && req.files.image2[0];
@@ -30,7 +30,7 @@ const addProduct= async(req,res)=>{
             category,
             price: Number(price),
             subCategory,
-            bestSeller : bestSeller === "true" ? true : false ,
+            bestSeller : bestseller === "true" ? true : false ,
             sizes : JSON.parse(sizes),
             image : imagesUrl,
             date: Date.now(),
@@ -39,7 +39,7 @@ const addProduct= async(req,res)=>{
         const product = new productModel(productData);
         await product.save();
 
-        console.log(name , description , price , category , subCategory , sizes , bestSeller);
+        console.log(name , description , price , category , subCategory , sizes , bestseller);
         console.log(productData);
         res.json({success: true, message: "Product Added"});
 
